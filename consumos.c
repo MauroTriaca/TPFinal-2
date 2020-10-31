f87c1753b98542d37cdd9e0fdfe09b9054763cd9
Mau <contact@mauweb.com>
2020-10-31 20:56:42 -0300

    Se agrego lo basico a libreria de consumos

--- consumos.c
+++ consumos.c
@@ -1,1 +1,93 @@
 #include "consumos.h"
+
+stConsumo cargaConsumoCliente()
+{
+    stConsumo g;
+    int baja=0;
+    int dias=0;
+
+
+
+        do{
+         printf("\ningrese El IdCliente.:");
+         scanf("%d", &g.idCliente);
+
+
+          if(g.idCliente<0 || g.idCliente>buscaUltimoId()){
+            printf("\n　Error,Ingrese un Id valido Por favor.\n");
+         }
+
+        }while(g.idCliente<0 || g.idCliente>buscaUltimoId());
+         ///Con esto se verifica que el cliente al que se le va a cargar un consumo exista
+        do{
+         printf("\ningrese El Consumo.:");
+         scanf("%d", &g.datosConsumidos);
+
+         if(g.datosConsumidos<0 || g.datosConsumidos>9999999){
+            printf("\n　Error,Ingrese un consumo valido Por favor.\n");
+         }
+        }while(g.datosConsumidos<0 || g.datosConsumidos>9999999);
+        ///con esto se verifica que el consumo sea positivo, y se le dio un tope el cual se supone que nunca se llegue
+        do{
+         printf("\ningrese el a%co......:",164);
+        scanf("%d", &g.anio);
+         if(g.anio!=2020){
+            printf("\n　Ese a%co no esta Disponible,Ingrese el a%co Actual Por favor.\n",164,164);
+         }
+
+        }while(g.anio!=2020);
+
+      do{
+         printf("\ningrese el mes.......:");
+         scanf("%d", &g.mes);
+          if(g.mes<1 || g.mes>6){
+            printf("\n　Ingrese Un mes Entre el 1(enero) al mes actual Por favor.\n");
+         }
+        }while(g.mes<1 || g.mes>6);
+
+        dias=CalcularDias(g.mes,g.anio);
+       do{
+         printf("\ningrese un dia.......:");
+         scanf("%d", &g.dia);
+            if(g.dia<1 || g.dia>dias){
+            printf("\n　Ingrese Un dia Entre el periodo de 1 a %d Por favor.\n",dias);
+         }
+
+         }while(g.dia<1 || g.dia>dias);
+         g.baja=baja;
+
+     ///Revisar todo lo refirido a existen Consumos.
+
+    return g;
+}
+
+stConsumo cargaConsumoAuto(){
+
+    stConsumo g;
+
+    int dia;
+
+    g.idCliente=rangoRandom(0,buscaUltimoId());
+
+    g.anio=2020;
+    g.mes=rangoRandom(1,7);
+    dia = CalcularDias(g.mes,g.anio);
+    g.dia=rangoRandom(1,dia);
+    g.datosConsumidos=rangoRandom(30,1000);
+
+ return g;
+}
+
+void mostrarUnConsumo(stConsumo g)
+{
+
+    printf("\n-----------------------\n");
+    printf("\nNumero Id........:%d",g.id);
+    printf("\nIdCliente........:%d",g.idCliente);
+    printf("\nDatos consumidos.:%d",g.datosConsumidos);
+    printf("\nA%co..............:%d",164,g.anio);
+    printf("\nMes..............:%d",g.mes);
+    printf("\nDia..............:%d",g.dia);
+    printf("\nBaja.............:%d\n",g.baja);
+    printf("\n-----------------------\n");
+}
--- consumos.h
+++ consumos.h
@@ -2,6 +2,21 @@
 #define CONSUMOS_H_INCLUDED
 #include "clientes.h"
 
+typedef struct
+{
+    int id;  /// campo único y autoincremental
+    int idCliente;
+    int anio;///actual
+    int mes; /// 1 a 12
+    int dia; /// 1 a … dependiendo del mes
+    int datosConsumidos;  /// expresados en mb.
+    int baja; /// 0 si está activo - 1 si está eliminado
+} stConsumo;
+
+stConsumo cargaConsumoCliente(); ///carga por teclado de un consumo
+stConsumo cargaConsumoAuto();///carga automatica un consumo, todo random
+void mostrarUnConsumo(stConsumo g);
+
 
 
 
